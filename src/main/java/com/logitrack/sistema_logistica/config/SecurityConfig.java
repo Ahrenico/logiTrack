607802094b0ba1c354dac3a670b4b8026b48286e
Grande-f <federicogrande2013@gmail.com>
2026-04-28 13:42:06 -0300

    Fix: corregir el manejo de errores en autenticación

--- src/main/java/com/logitrack/sistema_logistica/config/SecurityConfig.java
+++ src/main/java/com/logitrack/sistema_logistica/config/SecurityConfig.java
@@ -21,28 +21,17 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
 
-/*    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable()) // Deshabilitamos CSRF para facilitar pruebas iniciales
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll() // Permite entrar a todo sin login */
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            // Cada request se autentica con su propio token
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                 .requestMatchers("/api/auth/**").permitAll() 
-                 .requestMatchers("/api/envios/**").permitAll()
-                 // Rutas públicas: login y registro                
-                .anyRequest().authenticated() 
-                // Todo lo demás requiere token válido
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/envios/**").permitAll()
+                .anyRequest().authenticated()
             )
-            // Registramos nuestro filtro JWT antes del filtro de usuario/contraseña de Spring
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
