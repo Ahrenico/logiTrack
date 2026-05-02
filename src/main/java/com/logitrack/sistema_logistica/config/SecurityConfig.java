e0fa15f06ad3562790a4b410e4eb37f2f80410d5
Nicolas Rodriguez <nicolas.rodriguez336@gmail.com>
2026-05-01 21:29:37 -0300

    fix: Configuracion de cors

--- src/main/java/com/logitrack/sistema_logistica/config/SecurityConfig.java
+++ src/main/java/com/logitrack/sistema_logistica/config/SecurityConfig.java
@@ -9,11 +9,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.logitrack.sistema_logistica.security.JwtAuthFilter;
 
 import lombok.RequiredArgsConstructor;
-
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(org.springframework.security.config.Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -42,4 +46,26 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+        // ⚠️ IMPORTANTE: Acá poné la URL real de Vercel que te pasen los chicos de Front
+        configuration.setAllowedOrigins(Arrays.asList(
+            "https://tu-proyecto-front.vercel.app", 
+            "http://localhost:5500", 
+            "http://localhost:3000",
+            "http://localhost:5173" // Agregué el puerto 5173 por si usan Vite en local
+        ));
+        
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true); 
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    
 }
